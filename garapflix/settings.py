401433a0ea4e3e2b0d0fa50a6dacf5ea28f189d4
fineasdesign <franciscoguasco@gmail.com>
2026-05-04 01:48:25 -0300

    Alteração settings

--- garapflix/settings.py
+++ garapflix/settings.py
@@ -33,7 +33,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["projetogarapflixdjango-production.up.railway.app/", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://projetogarapflixdjango-production.up.railway.app/", "localhost", "127.0.0.1"]
 
 # Application definition
 
